6f2e732d1f505267f0ec5212ebbbadfeda0a6b40
Yarinisraelcohen <88779719+Yarinisraelcohen@users.noreply.github.com>
2022-08-16 17:01:21 +0300

    added new components and a cool spinner gif as well env. to get users from github

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,13 +6,27 @@ function Navbar({ title }) {
   return (
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
       <div className='container mx-auto'>
-        Navbar
         <div className='flex-none px-2 mx-2'>
-          <FaGithub className='inline pr-2 text -3xl' />
-          <Link to='/' className='text-lg font-bold align-middle'>
-            {' '}
-            {title}{' '}
+          <FaGithub className='inline pr-2 text-3xl' />
+          <Link to='/' className='text-lg text-white font-bold align-middle'>
+            {title}
           </Link>
+        </div>
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            <Link
+              to='/'
+              className=' text-white btn btn-ghost btn-sm rounded-btn'
+            >
+              Home
+            </Link>
+            <Link
+              to='/about'
+              className=' text-white btn btn-ghost btn-sm rounded-btn'
+            >
+              About
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
