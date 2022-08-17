e85210f5a1fee45e293692665eee80f800ddf8e4
Yarinisraelcohen <88779719+Yarinisraelcohen@users.noreply.github.com>
2022-08-17 13:01:45 +0300

    @_@

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import Spinner from '../layout/Spinner';
+import Useritem from './Useritem';
 function UserResults() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -20,11 +21,12 @@ function UserResults() {
     setUsers(data);
     setLoading(false);
   };
+
   if (!loading) {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map(user => (
-          <h3>{user.login}</h3>
+          <Useritem key={user.id} user={user} />
         ))}
       </div>
     );
@@ -32,5 +34,4 @@ function UserResults() {
     return <Spinner />;
   }
 }
-
 export default UserResults;
